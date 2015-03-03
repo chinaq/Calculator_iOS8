7a1f9df6cdf86aca28f3c22e55f88dbe619a3f54
chinaq <aaa6263@sina.com>
2015-03-03 21:35:11 +0800

    complete basic calculator function by lesson <2. More Xcode and Swift, MVC>

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,17 +9,73 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    @IBOutlet weak var display: UILabel!
+    var isUserInMiddleOfTyping = false
+    
+    //点击数字
+    @IBAction func appendDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        
+        if(isUserInMiddleOfTyping){
+            display.text = display.text! + digit
+        }else{
+            display.text = digit
+            isUserInMiddleOfTyping = true
+        }        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    //点击操作符，对栈中最后两个数进行计算
+    @IBAction func operate(sender: UIButton) {
+        let operation = sender.currentTitle!
+        if (isUserInMiddleOfTyping){
+            enter()
+        }
+        
+        switch operation{
+        case "×": performOperation {$0 * $1}
+        case "÷": performOperation {$1 / $0}
+        case "+": performOperation {$0 + $1}
+        case "−": performOperation {$1 - $0}
+        case "√": performOperation {sqrt($0)}
+        default: break
+        }
     }
-
-
+    
+    
+    func performOperation(operation:(Double, Double) -> Double){
+        if(operandStack.count >= 2){
+            displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
+            enter()
+        }
+    }
+    
+    func performOperation(operation:(Double) -> Double){
+        if(operandStack.count >= 1){
+            displayValue = operation(operandStack.removeLast())
+            enter()
+        }
+    }
+    
+    var operandStack = Array<Double>()   //数字栈
+    
+    //点击回车，将改数字追加到数字栈中
+    @IBAction func enter() {
+        isUserInMiddleOfTyping = false
+        operandStack.append(displayValue)
+        println("operandStack = \(operandStack)")
+    }
+    
+    //显示值的属性
+    var displayValue:Double{
+        get{
+            return NSNumberFormatter()
+                .numberFromString(display.text!)!.doubleValue
+        }
+        set{
+            display.text = "\(newValue)"
+            isUserInMiddleOfTyping = false
+        }
+    }
 }
 
